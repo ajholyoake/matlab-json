c03857c0384703b7874d36b403b249720ca2d5a9
Alexander Holyoake <alex.holyoake@caterhamf1.com>
2013-08-30 08:54:01 +0100

    Build options / isinf / isnan fixed

--- make.m
+++ make.m
@@ -1,7 +1,7 @@
 if ispc
     % Windows7/Visual Studio 2010
-    mex -ljson -g LINKFLAGS="$LINKFLAGS /NODEFAULTLIB:MSVSRT.lib /NODEFAULTLIB:LIBCMT.lib" -IC:\Users\panton\code\ -LC:\Users\panton\code\json\Release\ fromjson.c
-    mex -ljson -g LINKFLAGS="$LINKFLAGS /NODEFAULTLIB:MSVSRT.lib /NODEFAULTLIB:LIBCMT.lib" -IC:\Users\panton\code\ -LC:\Users\panton\code\json\Release\ tojson.c 
+    mex -ljson -g LINKFLAGS="$LINKFLAGS /NODEFAULTLIB:MSVSRT.lib /NODEFAULTLIB:LIBCMT.lib" -IC:\Users\alex.holyoake\Documents\html\json-c\ -LC:\Users\alex.holyoake\Documents\html\json-c\Release\ -L"C:\Program Files\ (x86)\Microsoft SDKs\Windows\v7.0A\" fromjson.c
+    mex -ljson -g LINKFLAGS="$LINKFLAGS /NODEFAULTLIB:MSVSRT.lib /NODEFAULTLIB:LIBCMT.lib" -IC:\Users\alex.holyoake\Documents\html\json-c\ -LC:\Users\alex.holyoake\Documents\html\json-c\Release\ -L"C:\Program Files\ (x86)\Microsoft SDKs\Windows\v7.0A\" tojson.c 
 else
     % Linux/Ubuntu/GCC
     mex -ljson -g fromjson.c
